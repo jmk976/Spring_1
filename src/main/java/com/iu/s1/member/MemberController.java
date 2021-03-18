30ed3e52651c38c628587670fe71d25ff6583c70
jmk976 <jmk976@gmail.com>
2021-03-18 14:46:17 +0900

    edit1

---
+++ src/main/java/com/iu/s1/member/MemberController.java
@@ -0,0 +1,43 @@
+package com.iu.s1.member;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MemberController {
+	
+	@Autowired
+	private memberService memberService; 
+	
+	//memberJoin print  //   /member/memberLogin
+	
+	@RequestMapping(value="/member/memberLogin")
+	public String memberJoin() {
+		System.out.println("로그인 하시오");
+		//WEB-INF/views/memberLogin.jsp
+		
+		return "member/memberLogin";
+	}
+	
+	//memberLogin2 print 들어온 것 만 확인
+	@RequestMapping(value ="/member/memberLogin", method = RequestMethod.POST)
+    public void memberLogin2(HttpServletRequest Request) throws Exception {
+		String id = Request.getParameter("id");
+		String pw = Request.getParameter("pw");
+		
+		
+		MemberDTO memberDTO = new MemberDTO();
+		memberDTO.setId(id);
+		memberDTO.setPw(pw);
+		
+		memberService.memberLogin(memberDTO);
+		
+		System.out.println(memberDTO);
+		
+    }
+
+}
